255a4583a1524f3892c7378523ad31c212da8871
maryayolansia <39435495+maryayolansia@users.noreply.github.com>
2019-07-11 13:40:46 +0700

    Update tambah.php

--- www/tambah.php
+++ www/tambah.php
@@ -1,6 +1,7 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Responsi TCC</title>
+    <title>Responsi Maria</title>
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
     <script type="text/javascript" src="assets/js/bootstrap.js"></script>
 </head>
@@ -48,5 +49,8 @@
     </div>
 </body>
 </html>
+
+
+
 
 
